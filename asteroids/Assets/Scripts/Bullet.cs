0f2c78e6e29c0c48c7243d06864ed15a080a5dce
ykupas <y.kupas.7@gmail.com>
2022-05-21 11:36:36 -0300

    Creating bullet script.

---
+++ asteroids/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Private variables
+    private Rigidbody2D _rigidbody;
+
+    // Public variables
+    public float bulletSpeed = 500.0f;
+    public float maxLifeTime = 10.0f;
+
+    // Awake is called at load time
+    private void Awake() 
+    {
+        // Attach ridigbody from Bullet GameObject
+        _rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Task that projects bullet called from player
+    public void Project(Vector2 direction)
+    {
+        // Add force to expecific direction 
+        _rigidbody.AddForce(direction * this.bulletSpeed);
+        // Destroy game object after maxLifeTime
+        Destroy(this.gameObject, this.maxLifeTime);
+    }
+}
