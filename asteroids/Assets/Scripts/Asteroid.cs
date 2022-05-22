b0cd8f916ae8f28392fb24f35a7f2f28d22c1e8f
ykupas <y.kupas.7@gmail.com>
2022-05-22 12:58:13 -0300

    Spawning random asteroid.

--- asteroids/Assets/Scripts/Asteroid.cs
+++ asteroids/Assets/Scripts/Asteroid.cs
@@ -6,8 +6,11 @@ public class Asteroid : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
 
-    // Public variables, like sprites
+    // Public variables, like sprites, size (used by spawner)
     public Sprite[] sprites;
+    public float size = 1.0f;
+    public float minSize = 0.5f;
+    public float maxSize = 1.5f;
 
     // Awake is called at load time
     private void Awake() 
@@ -21,9 +24,15 @@ public class Asteroid : MonoBehaviour
     private void Start() 
     {
         // Randomization of sprite selection
-        _spriteRenderer.sprite = this.sprites[Random.Range(0, tjis.sprites.length)];
+        _spriteRenderer.sprite = this.sprites[Random.Range(0, this.sprites.Length)];
         // Randomization of rotation
         this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
+
+        /* Randomization of size */
+        // Random scale of sprite and mass using size
+        // Vector3.one * this.size = new Vector3(this.size,this.size,this.size)
+        this.transform.localScale = Vector3.one * this.size;
+        _rigidbody.mass = this.size;
 
         
     }
