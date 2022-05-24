d98cfd4a094b3ec1f45c96a3943d0343894a5e8f
ykupas <y.kupas.7@gmail.com>
2022-05-23 21:31:48 -0300

    Spawning asteroids in different positions.

--- asteroids/Assets/Scripts/AsteroidSpawner.cs
+++ asteroids/Assets/Scripts/AsteroidSpawner.cs
@@ -3,20 +3,37 @@ using UnityEngine;
 public class AsteroidSpawner : MonoBehaviour
 {
     // Public variables
+    public Asteroid asteroidPrefab;
     public float spawnRate = 2.0f;
+    public float spawnDistance = 15.0f;
+    public int spawnAmount = 3;
 
 
     // Start is called before the first frame update
     private void Start()
     {
-        // Invoking Spawn function
+        // Invoking Spawn function periodically
         InvokeRepeating(nameof(Spawn), this.spawnRate, this.spawnRate);
     }
 
     // Spawn asteroid task
     private void Spawn()
     {
+        // Instantiate multiples asteroids
+        for(int i = 0 ; i < this.spawnAmount ; i++)
+        {
+            Vector3 random = Random.insideUnitCircle.normalized;
+            Debug.Log("Random value = " + random);
+            Vector3 spawnDirection = random * this.spawnDistance;
+            Debug.Log("Spawn direction value = " + spawnDirection);
+            Vector3 spawnPoint = this.transform.position + spawnDirection;
+            Debug.Log("Spawn point value = " + spawnPoint);
 
+            Quaternion spawnRotation = Quaternion.identity;
+
+            // Instantiate asteroid
+            Asteroid asteroid = Instantiate(asteroidPrefab, spawnPoint, spawnRotation);
+        }
     }
 
     // Update is called once per frame
