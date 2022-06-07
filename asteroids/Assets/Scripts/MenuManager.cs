b683d724977681b9ffdcdc08b2fbc2c88760fb14
ykupas <y.kupas.7@gmail.com>
2022-06-07 17:58:16 -0300

    Implementing interaction of start and quit buttons

---
+++ asteroids/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Load game scene function
+    public void LoadGame()
+    {
+        // Load next index scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quit game function
+    public void QuitGame()
+    {
+        // Quit game
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
