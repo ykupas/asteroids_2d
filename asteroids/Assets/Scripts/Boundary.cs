946df36c8144c1a81de5b41fb6da54de5579b1f3
ykupas <y.kupas.7@gmail.com>
2022-06-19 15:39:09 -0300

    Fixing problem of screen resolution and boundaries.

--- asteroids/Assets/Scripts/Boundary.cs
+++ asteroids/Assets/Scripts/Boundary.cs
@@ -15,12 +15,10 @@ public class Boundary : MonoBehaviour
         Vector3 viewPoint = new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z);
         // Pass it to world point Vector2
         Vector2 viewScreen = Camera.main.ScreenToWorldPoint(viewPoint);
-        Debug.Log(viewPoint);
-        Debug.Log(viewScreen);
         // Set boundaries positions
-        this.top.transform.position = new Vector3(viewScreen.x, this.top.transform.position.y, 0f);
-        this.bottom.transform.position = new Vector3(-viewScreen.x, this.bottom.transform.position.y, 0f);
-        this.right.transform.position = new Vector3(this.right.transform.position.x, viewScreen.y, 0f);
-        this.left.transform.position = new Vector3(this.left.transform.position.x, -viewScreen.y, 0f);
+        this.top.transform.position = new Vector3(0f, viewScreen.y, 0f);
+        this.bottom.transform.position = new Vector3(0f, -viewScreen.y, 0f);
+        this.right.transform.position = new Vector3(viewScreen.x, 0f, 0f);
+        this.left.transform.position = new Vector3(-viewScreen.x, 0f, 0f);
     }
 }
