ceea099e4ab99ce1492df00eea1c529864e2d765
ykupas <y.kupas.7@gmail.com>
2022-06-19 14:53:29 -0300

    Fixed problem, but created another one. When respawning from death, collisions are disabled, so he player can go out of boundaries.

--- asteroids/Assets/Scripts/Boundary.cs
+++ asteroids/Assets/Scripts/Boundary.cs
@@ -5,8 +5,8 @@ public class Boundary : MonoBehaviour
     // Public variables for boundaries
     public GameObject top;
     public GameObject bottom;
-    public GameObject left;
     public GameObject right;
+    public GameObject left;
 
     // Start is called before the first frame update
     public void Start()
@@ -16,12 +16,9 @@ public class Boundary : MonoBehaviour
         // Pass it to world point Vector2
         Vector2 viewScreen = Camera.main.ScreenToWorldPoint(viewPoint);
         // Set boundaries positions
-        // TODO
-    }
-
-    // Update is called once per frame
-    public void Update()
-    {
-        
+        this.top.transform.position = new Vector3(viewScreen.x, this.top.transform.position.y, 0f);
+        this.bottom.transform.position = new Vector3(-viewScreen.x, this.bottom.transform.position.y, 0f);
+        this.right.transform.position = new Vector3(this.right.transform.position.x, viewScreen.y, 0f);
+        this.left.transform.position = new Vector3(this.left.transform.position.x, -viewScreen.y, 0f);
     }
 }
